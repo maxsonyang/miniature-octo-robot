5558bf9b08db611fc8e05dcb305e84699007fdbf
maxsonyang <maxsonyang@berkeley.edu>
2021-06-29 11:09:17 -0700

    Update server/app.js
    Removing redundant comment
    
    Co-authored-by: ThinkTink <50761589+ThinkTink@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(express.static(join(__dirname, "public")));
 app.use(cookieparser(process.env.SESSION_SECRET));
 
 app.use(function (req, res, next) {
-  const token = req.cookies.token; // Get the cookie instead.
+  const token = req.cookies.token;
   if (token) {
     jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
       if (err) {
